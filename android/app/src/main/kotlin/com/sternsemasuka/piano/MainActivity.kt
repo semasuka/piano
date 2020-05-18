66ac4f388228906e0e73b9d94908197ebc9ba8db
stern15 <ssemasuka@gmail.com>
2020-05-18 12:02:32 -0600

    Initial commit & main piano keys layout

---
+++ android/app/src/main/kotlin/com/sternsemasuka/piano/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sternsemasuka.piano
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,179 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() {
+  runApp(Piano());
+}
+
+class Piano extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.grey,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 1.0),
+                  child: FractionallySizedBox(
+                    widthFactor: 0.95,
+                    child: FlatButton(
+                      splashColor: Colors.transparent,
+                      color: Colors.white,
+                      onPressed: () {
+                        print('key press');
+                      },
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(10.0),
+                          bottomRight: Radius.circular(10.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Stack(
+                children: <Widget>[
+                  Positioned(
+                    child: Container(
+                      width: 100,
+                      height: 100,
+                      color: Colors.red,
+                    ),
+                  )
+                ],
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 1.0),
+                  child: FractionallySizedBox(
+                    widthFactor: 0.95,
+                    child: FlatButton(
+                      splashColor: Colors.transparent,
+                      color: Colors.white,
+                      onPressed: () {
+                        print('key press');
+                      },
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(10.0),
+                          bottomRight: Radius.circular(10.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 1.0),
+                  child: FractionallySizedBox(
+                    widthFactor: 0.95,
+                    child: FlatButton(
+                      color: Colors.white,
+                      splashColor: Colors.transparent,
+                      onPressed: () {
+                        print('key press');
+                      },
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(10.0),
+                          bottomRight: Radius.circular(10.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 1.0),
+                  child: FractionallySizedBox(
+                    widthFactor: 0.95,
+                    child: FlatButton(
+                      splashColor: Colors.transparent,
+                      color: Colors.white,
+                      onPressed: () {
+                        print('key press');
+                      },
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(10.0),
+                          bottomRight: Radius.circular(10.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 1.0),
+                  child: FractionallySizedBox(
+                    widthFactor: 0.95,
+                    child: FlatButton(
+                      color: Colors.white,
+                      splashColor: Colors.transparent,
+                      onPressed: () {
+                        print('key press');
+                      },
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(10.0),
+                          bottomRight: Radius.circular(10.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 1.0),
+                  child: FractionallySizedBox(
+                    widthFactor: 0.95,
+                    child: FlatButton(
+                      color: Colors.white,
+                      splashColor: Colors.transparent,
+                      onPressed: () {
+                        print('key press');
+                      },
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(10.0),
+                          bottomRight: Radius.circular(10.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: FractionallySizedBox(
+                  widthFactor: 0.95,
+                  child: FlatButton(
+                    color: Colors.white,
+                    splashColor: Colors.transparent,
+                    onPressed: () {
+                      print('key press');
+                    },
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(10.0),
+                        bottomRight: Radius.circular(10.0),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
